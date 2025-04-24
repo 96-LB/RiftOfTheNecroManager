b2ffa729ace9e24c4358c1328eee16eaa94d7398
Lucas LaBuff <lucasbeef00@gmail.com>
2025-04-23 21:52:44 -0400

    add sample configs

--- LalaDancer/Config.cs
+++ LalaDancer/Config.cs
@@ -2,17 +2,32 @@
 
 namespace LalaDancer;
 
+public enum SampleEnum {
+    Foo,
+    Bar,
+    Baz
+}
+
 public static class Config {
     public class ConfigGroup(ConfigFile config, string group) {
-        public ConfigEntry<T> Bind<T>(string key, T defaultValue, string description) {
-            return config.Bind(group, key, defaultValue, description);
-        }
-
-        public ConfigEntry<T> Bind<T>(string key, T defaultValue, ConfigDescription description = null) {
-            return config.Bind(group, key, defaultValue, description);
+        public ConfigEntry<T> Bind<T>(string key, T defaultValue, string description, AcceptableValueBase acceptableValues = null, params object[] tags) {
+            return config.Bind(group, key, defaultValue, new ConfigDescription(description, acceptableValues, tags));
         }
     }
 
+    public static class Samples {
+        public static ConfigEntry<bool> SampleCheckbox { get; private set; }
+        public static ConfigEntry<SampleEnum> SampleCarousel { get; private set; }
+        public static ConfigEntry<int> SampleSlider { get; private set; }
+        public static void Initialize(ConfigGroup config) {
+            SampleCheckbox = config.Bind("Sample Checkbox", false, "Sample checkbox for boolean values.");
+            SampleCarousel = config.Bind("Sample Carousel", SampleEnum.Foo, "Sample carousel for enum values.");
+            SampleSlider = config.Bind("Sample Slider", 0, "Sample slider for integer values.", new AcceptableValueRange<int>(0, 100));
+        }
+    }
+
+
     public static void Initialize(ConfigFile config) {
+        Samples.Initialize(new(config, "Samples"));
     }
 }
