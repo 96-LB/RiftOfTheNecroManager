06f678465c3f380c170ef98e6622be93d80b680b
Lucas LaBuff <lucasbeef00@gmail.com>
2025-05-09 16:19:59 -0400

    add version check

--- RiftOfTheNecroManager/Plugin.cs
+++ RiftOfTheNecroManager/Plugin.cs
@@ -1,6 +1,8 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
+using Shared;
+using System.Linq;
 
 namespace RiftOfTheNecroManager;
 
@@ -10,20 +12,30 @@ public class Plugin : BaseUnityPlugin {
     const string GUID = "com.lalabuff.necrodancer.necromanager";
     const string NAME = "RiftOfTheNecroManager";
     const string VERSION = "0.0.1";
+    readonly static string[] BUILDS = ["1.4.0-b20638"];
 
     internal static ManualLogSource Log;
 
     internal void Awake() {
         Log = Logger;
 
+        var build = BuildInfoHelper.Instance.BuildId;
+        Log.LogInfo($"Current build info: {build} {BuildInfoHelper.Instance.CommitHash}");
+        if(!BUILDS.Contains(build)) {
+            Log.LogFatal($"The current version of the game is not compatible with this plugin. Please update the game or the mod to the correct version. The current mod version is v{VERSION} and the current game version is {build}. Allowed game versions: {string.Join(", ", BUILDS)}");
+            return;
+        }
+
+
+        RiftOfTheNecroManager.Config.Initialize(Config);
+
         Harmony harmony = new(GUID);
         harmony.PatchAll();
         
-        RiftOfTheNecroManager.Config.Initialize(Config);
-
-        Log.LogInfo($"{NAME} v{VERSION} ({GUID}) has been loaded! Have fun!");
         foreach(var x in harmony.GetPatchedMethods()) {
             Log.LogInfo($"Patched {x}.");
         }
+
+        Log.LogMessage($"{NAME} v{VERSION} ({GUID}) has been loaded! Have fun!");
     }
 }
