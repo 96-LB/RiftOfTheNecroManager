90c2cc3cd89a988873f83974599e90006b2fbeb4
Cap LaBuff <lucas.labuff@gmail.com>
2025-12-10 19:45:08 +0100

    bump version

--- RiftOfTheNecroManager/Plugin.cs
+++ RiftOfTheNecroManager/Plugin.cs
@@ -3,8 +3,8 @@
 namespace RiftOfTheNecroManager;
 
 
-[BepInPlugin(GUID, "RiftOfTheNecroManager", "0.2.8")]
+[BepInPlugin(GUID, "RiftOfTheNecroManager", "0.2.9")]
 public class Plugin : RiftPluginInternal {
     public const string GUID = "com.lalabuff.necrodancer.necromanager";
-    public override string AllowedVersions => "1.10.0 1.8.0 1.7.1 1.7.0 1.6.0 1.5.1 1.5.0 1.4.0";
+    public override string AllowedVersions => "1.11.1";
 }
