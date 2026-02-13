d6c0c6cc7a9807dd75b02571684ac6e438d5aeb1
Cap LaBuff <lucasbeef00@gmail.com>
2026-02-13 13:07:15 +0100

    whitespace

--- RiftOfTheNecroManager/ObjectExtensions.cs
+++ RiftOfTheNecroManager/ObjectExtensions.cs
@@ -7,11 +7,11 @@ public static class ObjectExtensions {
     public static void Pipe<T>(this T value, Action<T> func) {
         func(value);
     }
-
+    
     public static R Pipe<T, R>(this T value, Func<T, R> func) {
         return func(value);
     }
-
+    
     public static ref T Pipe<T>(this T value, ref T var) {
         var = value;
         return ref var;
